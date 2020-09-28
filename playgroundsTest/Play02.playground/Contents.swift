873d67e8ce55a8590a1c086d4308e41ed23e7a34
Néstor Moreno <ultranestor@hotmail.com>
2020-09-28 13:49:03 -0500

    Se añadió un playground con el ejemplo de tipos de enteros

--- playgroundsTest/Play02.playground/Contents.swift
+++ playgroundsTest/Play02.playground/Contents.swift
@@ -5,3 +5,19 @@ var welcomerMessage : String
 
 //Inicializamos el valor de welcome message
 welcomerMessage = "Valor inicial"
+
+var maxInt = UInt8.min
+var minInt = UInt8.max
+
+let integerMinU     = UInt.min
+let integerMaxU     = Int.max
+let integerMin8     = Int8.min
+let integerMax8     = Int8.max
+let integerMinU16   = UInt16.min
+let integerMaxU16   = UInt16.max
+let integerMin16    = Int16.min
+let integerMax16    = Int16.max
+let integerMaxU32   = UInt32.max
+
+
+
