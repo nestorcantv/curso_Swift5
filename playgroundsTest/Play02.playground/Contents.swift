f6e110b20da4ac0d40b635328813b1dbc40b7ebf
Néstor Moreno <ultranestor@hotmail.com>
2020-09-28 17:42:11 -0500

    Se modificó el playground Play02 y se añadió en su contenido ejemplos de TRY y CATCH

--- playgroundsTest/Play02.playground/Contents.swift
+++ playgroundsTest/Play02.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 var str = "Hello, playground"
 var welcomerMessage : String
 
-//Inicializamos el valor de welcome message
+/*Inicializamos el valor de welcome message
 welcomerMessage = "Valor inicial"
 
 var maxInt = UInt8.min
@@ -19,5 +19,60 @@ let integerMin16    = Int16.min
 let integerMax16    = Int16.max
 let integerMaxU32   = UInt32.max
 
+var prueba : UInt8 = 8
+var enteroDePrueba : Float32 = 8.99
+var resultado = prueba + UInt8(enteroDePrueba)
 
+var queOnda : String
+var queHubo = String()
+queOnda = "Que onda"
+queHubo = "Que Hubo?"
+print(queOnda, queHubo)
+
+// prueba de tupla
+var tupla = (StatusCode: 404, StatusCodeMessage: "Servidor no encontrado")
+let(e1,e2) = tupla
+print(tupla.StatusCode, tupla.StatusCodeMessage)
+let tuplaName = (name: "Néstor Josué" , lastName: "Moreno Palma" , age: 28)
+print("Su nombre es: \(tuplaName.name) \(tuplaName.lastName) y tienes \(tuplaName.age) años de edad")
+
+var test = "34"
+var testToInt = Int(test)
+print(testToInt!)*/
+
+var isAged : UInt8?
+
+
+isAged = 56
+if isAged != nil {
+    print("Es mayor de edad. Go ahead!! \(isAged!)")
+} else {
+    print("Usted no es mayor de edad. Usted tiene")
+}
+if let age = isAged{
+    print("Usted es mayor de edad. Usted tiene \(age)")
+}
+if let x = UInt16("1545"),
+    let y = UInt16("1545"), x == y {
+    let z = x + y
+    print(z)
+}
+
+
+
+func makeASandwich(statusHambre : String)throws{
+    let response = Bool(statusHambre)
+    if response == true{
+        print("Su sabdwich está en proceso de preparación.¡Buen provecho!")
+    }else if response == false {
+        print("Le notificaremos en una hora si gusta que le hagan un sandwich")
+    } else {
+        print("Ingrese True si tiene hambre o FALSE si no tiene apetito")
+    }
+}
+do{
+    try makeASandwich(statusHambre: "true")
+} catch {
+    print("Notificaremos en un hora si tiene hambre")
+}
 
